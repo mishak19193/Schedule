0ca85addc884429d6fce9aba1da59835e38672e1
19193 <19193@MXL52836SG.columbushs.com>
2018-08-22 14:43:18 -0400

    initial commit

---
+++ src/schedule/Schedule.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schedule;
+
+/**
+ *
+ * @author 19193
+ */
+public class Schedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        String firstName = "Michael";
+        String lastName = "Ishak";
+        int gradYear = 2019;
+        System.out.println("My name is " + firstName + " " + lastName + " and I will graduate in " + gradYear);
+        
+        String course1 = "AP Calculus";
+        String course2 = "AP Computer Science A";
+        String course3 = "AP Statistics";
+        String course4 = "Honors Social Justice";
+        String course5 = "AP Micro Economics";
+        String course6 = "Honors Mod. Middle Eastern History";
+        String course7 = "Honors English";
+        
+        String teacher1 = ("Mr. Ortega");
+        String teacher2 = ("Mr. Larrubia");
+        String teacher3 = ("Ms. Brown");
+        String teacher4 = ("Mr. Madri");
+        String teacher5 = ("Mr. Ciocca");
+        String teacher6 = ("Mr. Kumer");
+        String teacher7 = ("Mr. Linfors");
+      
+       System.out.println("1 " +course1+ " " +teacher1);
+       System.out.println("2 " +course2+ " " +teacher2);
+       System.out.println("3 " +course3+ " " +teacher3);
+       System.out.println("4 " +course4+ " " +teacher4);
+       System.out.println("5 " +course5+ " " +teacher5);
+       System.out.println("6 " +course6+ " " +teacher6);
+       System.out.println("7 " +course7+ " " +teacher7);
+    }
+    
+}
