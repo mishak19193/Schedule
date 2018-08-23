a97db66f19093411e71671b04247b95832716275
19193 <19193@MXL52836SG.columbushs.com>
2018-08-23 13:30:16 -0400

    changes spacing and borders

--- src/schedule/Schedule.java
+++ src/schedule/Schedule.java
@@ -38,13 +38,15 @@ public class Schedule {
         String teacher6 = ("Mr. Kumer");
         String teacher7 = ("Mr. Linfors");
       
-       System.out.println("1 " +course1+ " " +teacher1);
-       System.out.println("2 " +course2+ " " +teacher2);
-       System.out.println("3 " +course3+ " " +teacher3);
-       System.out.println("4 " +course4+ " " +teacher4);
-       System.out.println("5 " +course5+ " " +teacher5);
-       System.out.println("6 " +course6+ " " +teacher6);
-       System.out.println("7 " +course7+ " " +teacher7);
+       System.out.println("+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +");
+       System.out.println("1        " +course1+ "                               " +teacher1);
+       System.out.println("2        " +course2+ "                     " +teacher2);
+       System.out.println("3        " +course3+ "                             " +teacher3);
+       System.out.println("4        " +course4+ "                     " +teacher4);
+       System.out.println("5        " +course5+ "                        " +teacher5);
+       System.out.println("6        " +course6+ "        " +teacher6);
+       System.out.println("7        " +course7+ "                            " +teacher7);
+       System.out.println("+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - + ");
     }
     
 }
